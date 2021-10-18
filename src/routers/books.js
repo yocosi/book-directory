b8c38e8fa02e9745b794f6116c7f77eb2276221d
Pierre <pierre.amari46@gmail.com>
2021-10-18 22:24:46 +0200

    Adding Reading endpoints to find a book by his id or all books

--- src/routers/books.js
+++ src/routers/books.js
@@ -14,4 +14,34 @@ router.post('/books', async (req, res) => {
   }
 })
 
+// Reading endpoint to find all the books
+router.get('/books', async (req, res) => {
+  try {
+    const book = await Book.find({});
+
+    if (!book) {
+      return res.status(500).send({ error: "Unable to find books!" });
+    }
+    res.send(book);
+  } catch (error) {
+    res.status(500).send();
+  }
+})
+
+// Reading endpoint to find a book by id
+router.get('/books/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const book = await Book.findById(_id);
+
+    if (!book) {
+      return res.status(500).send();
+    }
+    res.send(book);
+  } catch (error) {
+    res.status(400).send({ error: "Unable to find the book!" });
+  }
+})
+
 module.exports = router;
