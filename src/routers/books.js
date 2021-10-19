c1e374c0962a724c2cf9a959f8e0d9c2ef8eed85
Pierre <pierre.amari46@gmail.com>
2021-10-19 20:36:08 +0200

    Add the Updating and Deleting endpoint by the book's ID

--- src/routers/books.js
+++ src/routers/books.js
@@ -44,4 +44,51 @@ router.get('/books/:id', async (req, res) => {
   }
 })
 
+// Updating endpoint to update a book by id
+router.patch('/books/:id', async (req, res) => {
+  const _id = req.params.id;
+  const originalBook = req.body;
+  const updates = Object.keys(originalBook);
+  const allowedUpdates = ['title', 'author', 'publisher', 'year'];
+  const isValidOperation = updates.every((update) => {
+    return allowedUpdates.includes(update);
+  })
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const book = await Book.findById(_id);
+    updates.forEach((update) => {
+      book[update] = originalBook[update];
+    })
+
+    await book.save();
+
+    if (!book) {
+      return res.status(404).send({ error: "Unable to find the book!" });
+    }
+    res.send(book);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
+
+// Deleting endpoint to delete a book item by ID
+router.delete('/books/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const book = await Book.findByIdAndDelete(_id);
+
+    if (!book) {
+      return res.status(404).send({ error: "Unable to find the book!" });
+    }
+    res.send(book);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
+
 module.exports = router;
