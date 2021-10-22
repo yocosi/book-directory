cf3cad68180f87f0559e4ef3feba54e094cb9a07
Pierre <pierre.amari46@gmail.com>
2021-10-22 21:49:28 +0200

    Adding the User model

---
+++ src/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  age: {
+    type: Number
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  }
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
