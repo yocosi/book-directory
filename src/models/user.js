ce3aa57df5f94dae9773129cf43a39dbcdae92b1
Pierre <pierre.amari46@gmail.com>
2021-10-22 22:38:02 +0200

    Adding bcryptjs npm module and use middlwares to automatically hash passwords when a user is created or when someone want to modify his password

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -27,6 +28,17 @@ const userSchema = new mongoose.Schema({
     trim: true,
     minlength: 6
   }
+})
+
+
+// MIDDLEWARES
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
 })
 
 const User = mongoose.model("User", userSchema);
